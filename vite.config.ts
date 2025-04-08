8911d01fd727c958160cd68e2d5d1ef7b2dffe4c
Tatiana Lagodich <t.lagodich@amarkets.com>
2025-04-08 18:11:18 +0400

    fix: add 404 for github actions

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
-import fs from 'fs';
-
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -12,6 +10,12 @@ export default defineConfig(({ mode }) => {
     base: env.VITE_BASE_URL || '/',
     build: {
       outDir: 'dist',
+      rollupOptions: {
+        input: {
+          main: 'index.html',
+          404: '404.html',
+        },
+      },
     },
     plugins: [
       vue({
